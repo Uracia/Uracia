e4212e887c9a06a3e2058a55fa271a6c46f836eb
Chan <135501329+invisableporxy@users.noreply.github.com>
2024-05-07 23:18:33 +0900

    Update 1.c
    Signed-off-by: Chan <135501329+invisableporxy@users.noreply.github.com>

--- 1.c
+++ 1.c
@@ -1,6 +1,6 @@
-#include “myProject.h"
-int printMyName1(void)
+#include "myProject.h"
+int printMyMame1(void)
 {
-  printf(“1. my name is [이찬웅]\n”); 
-return 1;
+    printf("1. my name is [이찬웅]\n");
+    return 1;
 }
