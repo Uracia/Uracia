4985fd086021973b58d342f83b82537f5e42ff04
tony0258 <130204182+tony0258@users.noreply.github.com>
2024-05-07 23:27:01 +0900

    Update 1.c
    Signed-off-by: tony0258 <130204182+tony0258@users.noreply.github.com>

--- 1.c
+++ 1.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
 #include "myProject.h"
-int printMyMame1(void)
+
+
+int printMyName1(void)
 {
-    printf("1. my name is [이찬웅]\n");
+    printf("1. my name is [이찬웅]\n"); // 올바른 이중 따옴표 사용
     return 1;
 }
