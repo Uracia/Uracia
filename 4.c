0d36fd0150af97a6cbc269e28e6b349222a5c79a
Chan <135501329+invisableporxy@users.noreply.github.com>
2024-05-07 23:31:17 +0900

    Update 4.c
    Signed-off-by: Chan <135501329+invisableporxy@users.noreply.github.com>

--- 4.c
+++ 4.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include "myProject.h"
 
-
-int printMyName4(void)
-{
-    printf("4. my name is [정현준]\n"); 
-    return 1;
+void printMyName4() {
+    printf("4. my name is [정현준]\n");
 }
