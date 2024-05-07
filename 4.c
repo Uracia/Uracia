19a0d3f7a3fed53d16fe21b9648cb2ec65cb430e
tony0258 <130204182+tony0258@users.noreply.github.com>
2024-05-07 23:28:23 +0900

    Update 4.c
    Signed-off-by: tony0258 <130204182+tony0258@users.noreply.github.com>

--- 4.c
+++ 4.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
 #include "myProject.h"
-int printMyMame4(void)
+
+
+int printMyName4(void)
 {
-    printf("4. my name is [정현준]\n");
+    printf("4. my name is [정현준]\n"); 
     return 1;
 }
