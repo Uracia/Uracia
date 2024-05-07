b7bc18d8045cd4af17fcffb3d9ba8b4a5caf34e4
tony0258 <130204182+tony0258@users.noreply.github.com>
2024-05-07 23:28:51 +0900

    Update myProject.h
    Signed-off-by: tony0258 <130204182+tony0258@users.noreply.github.com>

--- myProject.h
+++ myProject.h
@@ -1,4 +1,11 @@
-#include <stdio.h>
+#ifndef myProject_h
+#define myProject_h
+
+
+
 int printMyName1(void);
+int printMyName2(void);
+int printMyName3(void);
+int printMyName4(void);
 
-
+#endif // MY_PROJECT_H
